73619bfe4428bc8a129fefee4a08876734ead432
HakureiReimuOVO <HakureiReimuOVO@outlook.com>
2023-12-26 15:23:06 +0800

    feat: filter models with empty tags

--- server.py
+++ server.py
@@ -37,10 +37,11 @@ def query_model_tags():
     try:
         data = request.args
         fields = data.get('fields').split(',')
-        model_num = int(data.get('model_num', 5))
+        model_num = int(data.get('model_num', 10))
         tag_num = int(data.get('tag_num', 5))
         similar_models = milvus_dao.query_models_by_fields(fields=fields, model_num=model_num)
-        tags = recommend_tags(models=similar_models, tag_num=tag_num)
+        filtered_similar_models = list(filter(lambda model: model['tags'] and model['tags'] != [''], similar_models))
+        tags = recommend_tags(models=filtered_similar_models, tag_num=tag_num)
         return jsonify({'message': 'ok', 'code': 200, 'data': {'tags': tags}})
     except Exception as e:
         return jsonify({'message': e, 'code': 400})
@@ -48,4 +49,4 @@ def query_model_tags():
 
 if __name__ == '__main__':
     milvus_dao = MilvusDAO()
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
